7701c65143dce9926fcfb942fa9d3a2bec3cb23c
Divyansh044 <divyanshbangwal@gmail.com>
2025-05-01 12:27:51 +0530

    necessary updates

--- Sspotipy/main.py
+++
@@ -1,7 +0,0 @@
-import os
-from flask import Flask
-app = Flask(__name__)
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Dynamically use Railway's port
-    app.run(host='0.0.0.0', port=port, debug=True)
